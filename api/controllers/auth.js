fdb5e05d8758c8ca65295df612a1c4689b00ebdb
ahmed nasser <ahmednas197@gmail.com>
2022-08-26 18:34:17 +0200

    Add authentication using JWT

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import Users from "../models/Users.js"
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import { createError } from '../utils/error.js';
 export const register = async (req, res, next) => {
     try {
@@ -26,14 +27,18 @@ export const login = async (req, res, next) => {
     try {
         //check if the user exits
         const user = await Users.findOne({ username: req.body.username })
-        console.log("USer", user)
         if (!user) return next(createError(404, "User Not Found"));
 
         //check if the password is correct 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if (!isPasswordCorrect) return next(createError(404, "Wrong password or username"));
 
-        res.status(200).send(user)
+
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
+        const { password, isAdmin, ...otherDetails } = user._doc
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        }).status(200).send({ ...otherDetails })
 
 
     } catch (err) {
--- api/index.js
+++ api/index.js
@@ -5,9 +5,7 @@ import authRoute from './routes/auth.js'
 import usersRoute from './routes/users.js'
 import hotelsRoute from './routes/hotels.js'
 import roomsRoute from './routes/rooms.js'
-
-
-
+import cookieParser from 'cookie-parser'
 const app = express();
 dotenv.config();
 
@@ -31,6 +29,7 @@ mongoose.connection.on("disconnected", () => {
 
 
 //middleware
+app.use(cookieParser())
 app.use(express.json())
 app.use("/api/v1/auth", authRoute)
 app.use("/api/v1/users", usersRoute)
