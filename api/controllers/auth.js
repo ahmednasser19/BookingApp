af0c1ef34f5d72bc75ba0de79b474f9625e6033e
ahmed nasser <ahmednas197@gmail.com>
2022-08-26 19:23:34 +0200

    JWT auth verification

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,4 @@
-import Users from "../models/Users.js"
+import Users from "../models/User.js"
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { createError } from '../utils/error.js';
@@ -36,10 +36,12 @@ export const login = async (req, res, next) => {
 
         const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
         const { password, isAdmin, ...otherDetails } = user._doc
-        res.cookie("access_token", token, {
-            httpOnly: true,
-        }).status(200).send({ ...otherDetails })
-
+        res
+            .cookie("access_token", token, {
+                httpOnly: true,
+            })
+            .status(200)
+            .send({ ...otherDetails });
 
     } catch (err) {
         next(err)
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,24 @@
 import express from 'express'
+import { updateUser, deleteUser, getUser, getUsers } from '../controllers/user.js';
+import { verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.send("Hello, this is auth endpoint")
-})
-router.get("/register", (req, res) => {
-    res.send("Hello, this is auth endpoint")
+router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+    res.send("Hello user")
 })
 
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("Hello user you are loged in ")
+})
+//UPDATE
+router.put("/:id", updateUser)
+//DELETE
+router.delete("/:id", deleteUser)
+//GET
+router.get("/:id", getUser)
+//GET ALL
+router.get("/", getUsers)
 
 export default router
